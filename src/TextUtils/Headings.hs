9858df110f07faf237142268bd09fd39c71db51d
someodd <someodd@pm.me>
2021-01-22 02:47:01 -0800

    Implement font files in text files

--- src/TextUtils/Headings.hs
+++ src/TextUtils/Headings.hs
@@ -22,6 +22,10 @@ import qualified Data.Map as Map
 type AsciiFont = Map.Map Char [String]
 
 
+-- FIXME: is it bad that it doesnt autopad? also in the header you should be able to define the definign width/height character
+-- if do autopad but maybe that's all bad idea
+-- FIXME: add a header to the file that's just a comma-delimited list of settings like uppercase or lowercase.
+-- this will require makign AsciiFont a record.
 -- FIXME: do consistency checking OR padd them out! choose one!
 -- | Create an AsciiFont from a ".bmf` file (burrow monospaced font)...
 parseFont :: FilePath -> IO AsciiFont
@@ -38,6 +42,14 @@ parseFont path = do
           c:[] -> (c, xs)
           _ -> error "Legend in char legend is not just a single character."
       _ -> error "Char legend misformatted."
+
+
+-- TODO: In the future a config/ini file will specify which fonts get mapped to what. In this
+-- same cnofig other settings will be named.
+getAsciiFonts :: IO (Map.Map String AsciiFont)
+getAsciiFonts = do
+  h1 <- parseFont "data/fonts/basicthorpe.bmf"
+  pure $ Map.fromList [("h1", h1)]
 
 
 -- Need to make it so you can actually load in fonts from files
@@ -69,22 +81,34 @@ h1 =
     Map.fromList [('a', a), ('b', b), ('c', c)]
 
 
+-- this is hacks in case... update when ini later
 fontLookup :: Char -> AsciiFont -> Maybe [String]
-fontLookup c = Map.lookup (toLower c)
+fontLookup c f =
+  case Map.lookup (toLower c) f of
+    Nothing ->
+      case Map.lookup (toUpper c) f of
+        Nothing -> Nothing
+        x -> x
+    x -> x
 
 
 fontWidthHeight :: AsciiFont -> (Int, Int)
 fontWidthHeight font =
+  -- temporary hack FIXME (see notes about ini)
   case Map.lookup 'a' font of
-    Just [] -> error "Font letter 'a' is blank: []!"
-    Just a ->
-      let
-        firstLineLength = length (head a)
-      in
-        if firstLineLength < 1
-          then error "First line length <0"
-          else (length $ head a, length a)
-    Nothing -> error "Font does not have 'a'!"
+    Nothing ->
+      case Map.lookup 'A' font of
+        Nothing -> error "Font width/height error: neither 'a' nor 'A' defined."
+        Just a -> determineWidthHeightFromA a
+    Just a -> determineWidthHeightFromA a
+ where
+  determineWidthHeightFromA a =
+    let
+      firstLineLength = length (head a)
+    in
+      if firstLineLength < 1
+        then error "First line length <0"
+        else (length $ head a, length a)
 
 
 headingCompose :: AsciiFont -> String -> String
