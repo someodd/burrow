a5ab9225bab9c848ca2541b572f23c8364456448
Hyperreal Gopher <hyperrealgopher@protonmail.ch>
2021-01-30 21:24:43 -0800

    selective import/export on Types

--- src/Types.hs
+++ src/Types.hs
@@ -1,9 +1,9 @@
-module Types where
+module Types (ParseEnv) where
 
-import Control.Monad.Reader
-import Data.Map as Map
+import Control.Monad.Reader (Reader)
+import qualified Data.Map as Map
 
-import TextUtils.Headings
+import TextUtils.Headings (AsciiFont)
 
 -- | Make the ascii art font files available to a commonmark parser.
 type ParseEnv a = Reader (Map.Map String AsciiFont) a
