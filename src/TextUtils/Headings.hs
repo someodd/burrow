0c117e5ff42332215434bac02d3a0c27251e6393
Hyperreal Gopher <hyperrealgopher@protonmail.ch>
2021-01-31 22:51:55 -0800

    clean up headings module a little, add docs

--- src/TextUtils/Headings.hs
+++ src/TextUtils/Headings.hs
@@ -1,6 +1,9 @@
--- | Create fancy ASCII headings from a string.
+-- | Create fancy ASCII-art-type text from an input string.
+--
+-- Burrow has support for its own font specification, which is used
+-- just for creating ASCII-art headings.
 
-module TextUtils.Headings where
+module TextUtils.Headings (AsciiFont, headingCompose, getAsciiFonts) where
 
 import Data.Char
 import Data.List (transpose, intercalate)
@@ -10,24 +13,19 @@ import Data.List.Split (splitOn)
 import qualified Data.Map as Map
 
 
--- Should also have functions for creating cooler fonts than just monospaced ones.
+-- | An ASCII art character is represented by breaking up each line into an element of a list.
+type AsciiChar = [String]
+
+-- | An ASCII font is represented this way to make it easy to lookup a regular character and
+-- get the AsciiChar in return.
+type AsciiFont = Map.Map Char AsciiChar
 
 
--- | Map a character to a bunch of lines.
+-- | Parse a Burrow ASCII art font file.
 --
--- It's important that each char has equal chars per row.
+-- This spec is subject to change. Currently only supports monospaced fonts.
 --
--- You must have the letter 'a' defined, at least. It is used for
--- determining the width and height of the rest of the characters.
-type AsciiFont = Map.Map Char [String]
-
-
--- FIXME: is it bad that it doesnt autopad? also in the header you should be able to define the definign width/height character
--- if do autopad but maybe that's all bad idea
--- FIXME: add a header to the file that's just a comma-delimited list of settings like uppercase or lowercase.
--- this will require makign AsciiFont a record.
--- FIXME: do consistency checking OR padd them out! choose one!
--- | Create an AsciiFont from a ".bmf` file (burrow monospaced font)...
+-- See the project's README for the font spec.
 parseFont :: FilePath -> IO AsciiFont
 parseFont path = do
   fontFileContents <- readFile path
@@ -44,41 +42,16 @@ parseFont path = do
       _ -> error "Char legend misformatted."
 
 
--- TODO: In the future a config/ini file will specify which fonts get mapped to what. In this
--- same cnofig other settings will be named.
+-- | Currently a hardcoded reading-in of AsciiFont files, mapping to
+-- their heading level name.
+--
+-- This will be updated in the future to set the various heading levels to specific
+-- font locations via a config.
 getAsciiFonts :: IO (Map.Map String AsciiFont)
 getAsciiFonts = do
   h1 <- parseFont "data/fonts/basicthorpe.bmf"
+  -- TODO: in the future I think this should be the heading level as an integer.
   pure $ Map.fromList [("h1", h1)]
-
-
--- Need to make it so you can actually load in fonts from files
-h1 :: AsciiFont
-h1 =
-  let
-    a =
-      [ " # "
-      , "# #"
-      , "###"
-      , "# #"
-      , "# #"
-      ]
-    b =
-      [ "## "
-      , "# #"
-      , "###"
-      , "# #"
-      , "###"
-      ]
-    c =
-      [ "###"
-      , "#  "
-      , "#  "
-      , "#  "
-      , "###"
-      ]
-  in
-    Map.fromList [('a', a), ('b', b), ('c', c)]
 
 
 -- this is hacks in case... update when ini later
