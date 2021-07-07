63a006a091dbc382afa6423db98a3fa16a41e184
Hyperreal Gopher <hyperrealgopher@protonmail.ch>
2021-07-07 02:05:58 -0700

    clean up command interface (build subcommand)

--- app/Main.hs
+++ app/Main.hs
@@ -8,44 +8,44 @@ import Options.Applicative hiding (ParseError)
 import Paths_burrow (version)
 import Data.Version (showVersion)
 
--- | Model for the CLI options
--- usage: burrow.hs source-dir-or-file --version --help --destination=some-dir
--- if out-dir is specified, output to directory and not std-out. must be specified
--- if source is directory. specify destination to not use stdout or if
--- the source is a directory
-data BuildOptions = BuildOptions
-  { buildSpacecookie :: Bool -- Use .gopher
-  }
+-- bad practice (should have options be a separate type
+data SubCommand
+  = Build { buildSpacecookie :: Bool }
+  deriving Show
 
-data Command
-  = Build BuildOptions
+-- | The main CLI interface for the main CLI parser.
+data MainOptions = MainOptions
+  { subcommand :: SubCommand
+  } deriving Show
 
+mainCommand :: Parser MainOptions
+mainCommand = MainOptions <$> subCommands
 
-data MainInterface = { mainCommand :: Command }
+versionOption :: Parser (a -> a)
+versionOption = infoOption (showVersion version) (long "version" <> help "Show version")
 
+subCommands :: Parser SubCommand
+subCommands =
+  hsubparser
+    ( command "build" (info buildSubCommand
+        ( progDesc "Create gopherhole according to gopherhole.ini" ))
+    )
+
+buildSubCommand :: Parser SubCommand
+buildSubCommand = Build <$>
+  ( switch
+      (long "spacecookie" <> help "Parse index.md.mustache files to .gophermap files")
+  )
+
+mainParser :: ParserInfo MainOptions
+mainParser = info (helper <*> versionOption <*> mainCommand)
+      (fullDesc <> progDesc ("burrow v" <> (showVersion version)) <>
+       header "Build elaborate static gopherholes using Mustache and Markdown.")
+
+-- FIXME: show help by default if no action specified
 -- FIXME: make it so build is only one of many options/actions. you can also create phlog posts and list tags. Use a phlog post template?
--- TODO/FIXME:
--- Need to instead glob the input directory for *.txt.mustache first, then *.md.mustache
--- in order to create directories and files for gopherspace, respectively.
 main :: IO ()
 main = do
-  opts <- execParser parser
-  case opts of
-    Build buildOptions -> buildGopherhole (buildSpacecookie buildOptions)
- where
-  parser :: ParserInfo MainInterface
-  parser =
-    info
-      (helper <*> versionOption <*> (Build <$> buildParser))
-      (fullDesc <>        header "burrow: build gopherholes using Mustache and Markdown")
-
-  versionOption :: Parser (a -> a)
-  versionOption = infoOption (showVersion version) (long "version" <> help "Show version")
-
-  buildParser =
-    hsubparser (command "build" (info buildOptions (progDesc "Build a gopherhole according to gopherhole.ini")))
-   where
-    buildOptions =
-      BuildOptions <$>
-      switch
-        (long "spacecookie" <> help "Parse index.md.mustache files to .gophermap files")
+  opts <- execParser mainParser
+  case subcommand opts of
+    Build buildOptions -> buildGopherhole (buildOptions)
