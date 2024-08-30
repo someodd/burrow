4c9968abb1c23e3fac335b2d63c8f764f08477f4
someodd <someodd@pm.me>
2024-08-30 16:43:58 -0700

    remove verbosity

--- docker/auto_update.sh
+++ docker/auto_update.sh
@@ -3,8 +3,6 @@
 # Script for automatically updating Burrow.
 set -e
 
-echo "auto update script"
-
 # Local repository path (this path is hardcoded)
 LOCAL_GOPHERHOLE_REPO_PATH="/srv/git/gopherhole.git"
 
@@ -23,9 +21,6 @@ if [[ "${AUTO_BURROW_UPGRADE}" == "true" ]]; then
 
     # Compare the local and latest Burrow versions
     if [ "$LOCAL_BURROW_VERSION" != "$LATEST_BURROW_VERSION" ]; then
-        echo "The local Burrow version is NOT up-to-date with the latest release."
-        echo "Local Burrow version: $LOCAL_BURROW_VERSION"
-        echo "Latest Burrow version: $LATEST_BURROW_VERSION"
         /usr/local/bin/install-latest-deb.sh
     fi
 fi
@@ -35,7 +30,6 @@ fi
 git config --global --add safe.directory "$LOCAL_GOPHERHOLE_REPO_PATH"
 
 is_remote_newer() {
-    echo "checking if remote is newer"
     # Return 0 if the remote repository is newer than the local repository, 1 otherwise.
     local local_repo="$1"
     local remote_repo="$2"
@@ -54,7 +48,6 @@ is_remote_newer() {
     fi
 
     # Get the latest commit timestamp of the remote repository
-    echo "try date"
     remote_date=$(mkdir -p /tmp/temp-repo && \
                   cd /tmp/temp-repo && \
                   git init -q && \
@@ -62,7 +55,6 @@ is_remote_newer() {
                   git log -1 --format=%ct FETCH_HEAD && \
                   cd - > /dev/null && \
                   rm -rf /tmp/temp-repo)
-    echo "got date"
 
     # Compare timestamps and return True if the remote is newer
     if [[ "$remote_date" -gt "$local_date" ]]; then
@@ -72,14 +64,11 @@ is_remote_newer() {
     fi
 }
 
-echo "rest of script: checking if remote is newer"
 cd "$LOCAL_GOPHERHOLE_REPO_PATH"
 is_remote_newer "$LOCAL_GOPHERHOLE_REPO_PATH" "$GOPHERHOLE_REMOTE_URL" "$GOPHERHOLE_REMOTE_BRANCH"
 is_newer=$?
-echo "status of is_newer: $is_newer"
 
 if [[ -n $GOPHERHOLE_REMOTE_URL ]] && [[ $is_newer -eq 0 ]]; then
-    echo "Remote repository is newer. Start sync."
     rm -rf /tmp/gopherhole-clone
     git fetch "$GOPHERHOLE_REMOTE_URL" "$GOPHERHOLE_REMOTE_BRANCH:$GOPHERHOLE_REMOTE_BRANCH" --force
     "$LOCAL_GOPHERHOLE_REPO_PATH/hooks/post-receive"
